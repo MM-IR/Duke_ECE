718f01222efc54a593139a80175033025d31f4c3
Randy_wangjy <ttwjytt@outlook.com>
2020-04-28 18:13:04 +0800

    068

---
+++ 068_circle/circle.h
@@ -0,0 +1,11 @@
+#include "point.h"
+
+class Circle {
+private:
+	Point center;
+	const double radius;
+public:
+	Circle(Point c, double r) : center(c), radius(r) {}
+	void move(double dx, double dy);
+	double intersectionArea(const Circle &otherCircle);
+};
