d43c5df192df0e92a37c09432e828ece3c0be731
Randy_wangjy <ttwjytt@outlook.com>
2020-05-03 19:45:26 +0800

    093

---
+++ 093_binsrch/search.cpp
@@ -0,0 +1,25 @@
+#include "function.h"
+#include <iostream>
+
+int binarySearchForZero(Function<int, int> * f, int low, int high)
+{
+  if (f->invoke(high-1)<0){
+    return high-1;
+  }
+  else if(f->invoke(low) > 0){
+    return low;
+  }
+  while (low < high){
+    int mid = (high - low)/2 + low;
+    if (f->invoke(mid)>0){
+      high = mid;
+    }
+    else if (f->invoke(mid) < 0){
+      low = mid + 1;
+    }
+    else{
+      return mid;
+    }
+  }
+  return low;
+}
